89ce776d89d3413f822cfdd30f954baf02367b27
newpanjing <newpanjing@163.com>
2019-07-01 16:29:44 +0800

    分页

--- device/admin.py
+++ device/admin.py
@@ -9,6 +9,7 @@ class DeviceAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'device_no', 'type', 'create_date')
     list_filter = ('type',)
     search_fields = ('name',)
+    list_per_page = 20
 
 
 @admin.register(Firmware)
@@ -16,8 +17,10 @@ class FirmwareAdmin(admin.ModelAdmin):
     list_filter = ('id', 'device', 'identifier', 'version', 'url', 'releasedate', 'uploaddate', 'signed', 'create_date')
     list_filter = ('device', 'signed')
     search_fields = ('version',)
+    list_per_page = 20
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('name', 'update_date', 'file')
+    list_per_page = 20
